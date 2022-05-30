63df8cd7279a214aff84558bc94fe41c5c32fb24
GameboyVlol <105992074+GameboyVlol@users.noreply.github.com>
2022-05-31 00:14:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from aiohttp import ClientSession
 
 b=commands.Bot(command_prefix='=', self_bot=True, help_command=None)
 
-TOKEN = #enter your accounts token here
+TOKEN = OTgwOTUyNjU5NDUwMjg2MDkx.GnxuvP.zQ7Ne_CvVgJqgKFcgwYL2ROnO0CGuAy4949iBU
 
 @b.event
 async def on_connect():
@@ -15,11 +15,11 @@ async def on_connect():
 async def on_message(ctx):
   if ctx.channel.id==915274436347826227:
     if ctx.author.id==700275401314009168:
-      channel=b.get_channel() #enter the id of the channel you want to have the bot send its messages to
+      channel=b.get_channel() 980950340096655390
       content=str(ctx.content).replace("@", "[at]")
       await channel.send(content)
     else:
-      WEBHOOK_URL="" #enter your webhook url here
+      WEBHOOK_URL="" https://discord.com/api/webhooks/980957007177723955/eoDkWsGwunyVoolTY2CQ7IPfZ9GYJ-cIHxylLAAZJ6NePADU3Stfm8_NgR9rQspbTn60
       async with ClientSession() as session:
         webhook = discord.Webhook.from_url(WEBHOOK_URL, adapter=discord.AsyncWebhookAdapter(session))
         await webhook.send(content=str(ctx.content).replace("@", "[at]"), username=ctx.author.name, avatar_url=ctx.author.avatar_url)
